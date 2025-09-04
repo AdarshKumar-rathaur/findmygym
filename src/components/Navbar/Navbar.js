5b704d4deb328e9fb860fd7337f8b3e5445b8954
AdarshKumar-rathaur <72179963+AdarshKumar-rathaur@users.noreply.github.com>
2025-09-04 17:31:26 +0530

    make city selectable

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,41 +1,46 @@
-import React from 'react'
-import './Navbar.css';
+import React from "react";
+import "./Navbar.css";
 
 export default function Navbar(props) {
-    return (
-        <header>
-            <div className="navbar">
-                <div className="nav_content left">
-                    <div className="nav-logo">
-                        <div className="logo">
-                            <i className="fa-solid fa-person-running" />
-                        </div>
-                        <div className="brand_name">
-                            <span id="solid">FIND</span><span id="thin">MYGYM</span>
-                        </div>
-                    </div>
-                    <div className="nav-search">
-                        <input name='search' type="search" placeholder="Search" className='search-input' />
-                        <div className="search-icon">
-                            <i className="fa-solid fa-magnifying-glass" />
-                        </div>
-                    </div>
-                </div>
-                <div className="nav_content right">
-                    <div className="userAddress">
-                        <div className="userAddress-name" id="solid">
-                            {props.location}
-                        </div>
-                        <div className="userAddress-change">
-                            <i className="fa-solid fa-angle-down" />
-                        </div>
-                    </div>
-                    <div className="login">
-                        <button className="login-button">Sign In</button>
-                    </div>
-                </div>
+  return (
+    <header>
+      <div className="navbar">
+        <div className="nav_content left">
+          <div className="nav-logo">
+            <div className="logo">
+              <i className="fa-solid fa-person-running" />
             </div>
-        </header >
-    )
+            <div className="brand_name">
+              <span id="solid">FIND</span>
+              <span id="thin">MYGYM</span>
+            </div>
+          </div>
+          <div className="nav-search">
+            <input
+              name="search"
+              type="search"
+              placeholder="Search"
+              className="search-input"
+            />
+            <div className="search-icon">
+              <i className="fa-solid fa-magnifying-glass" />
+            </div>
+          </div>
+        </div>
+        <div className="nav_content right">
+          <form name="userAddress">
+            <select className="userAddress">
+              <option>{props.location}</option>
+              <option value="noida">Noida</option>
+              <option value="greaterNoida">Greater Noida</option>
+              <option value="ghaziabad">Ghaziabad</option>
+            </select>
+          </form>
+          <div className="login">
+            <button className="login-button">Sign In</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
-
